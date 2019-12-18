f9a4e2af127954a0b106b5b4ae67f0be3f878529
Yusuf Felly <beebarss.saj.ma@gmail.com>
2019-12-18 15:16:59 +0100

    Added comments in year.py

--- input2/year.py
+++ input2/year.py
@@ -1,7 +1,12 @@
 import datetime
 
+""" This is a simple python program that calculates the age based on user input. """
+
 
 def display_year(input_display):
+    """ This is the main function that displays the calculated age based on the value entered by the
+    user. """
+
     dic_values = calculate_year(input_display)
     if dic_values["error"]:
         print(dic_values["error"])
@@ -12,17 +17,21 @@ def display_year(input_display):
             + " years old"
             + ", and you were born in the "
             + str(dic_values["century"])
-            + " century"
+            + " century!"
         )
 
 
 def calculate_year(input_year):
+    """ This function is where the actual calculations are made.
+    Before doing any calculations it calls validate_year() to verify the entered values. """
+
     now = datetime.datetime.now()
     year_now = now.year
     year_now_trimmed = str(year_now)[2:]
     # print(year_now_trimmed)
 
     dic = {}
+    dic["error"] = ""
 
     form = validate_year(input_year)
     # print(form)
@@ -30,7 +39,7 @@ def calculate_year(input_year):
     if form != "Malformed input!":
 
         if len(form) == 4:
-            dic["nb_years"] = now.year - input_year
+            dic["nb_years"] = now.year - int(input_year)
 
             if form == "19XX":
                 dic["nb_years"] = now.year - input_year
@@ -40,13 +49,13 @@ def calculate_year(input_year):
                 dic["century"] = "21st"
 
             elif form == "XXXX":
-                dic["nb_years"] = now.year - input_year
-                dic["century"] = str(int(str(input_year)[:2]) - 1)
+                dic["nb_years"] = now.year - int(input_year)
+                dic["century"] = int(str(input_year)[:2]) - 1
             else:
                 dic["error"] = "Error"
 
         elif form == "XX":
-            if int(year_now_trimmed) <= input_year:
+            if int(year_now_trimmed) <= int(input_year):
                 converted = int("19" + str(input_year))
                 dic["nb_years"] = now.year - converted
                 dic["century"] = "20th"
@@ -64,7 +73,9 @@ def calculate_year(input_year):
 
 
 def validate_year(user_input):
-    year = user_input
+    """ This functions takes in the user input and verify it according to a set of criteria. """
+
+    year = str(user_input)
     # print(year)
     # print(str(year)[:2])
     # print(len(str(year)))
@@ -87,10 +98,14 @@ def validate_year(user_input):
             form = "Unknown"
         # print(form)
     else:
-        form = "Malformed input!"
+        # Showing an error if the entered string contains letters
+        try:
+            int(year)
+        except:
+            form = "Malformed input!"
 
     return form
 
 
 if __name__ == "__main__":
-    display_year("d558")
+    display_year(74)
